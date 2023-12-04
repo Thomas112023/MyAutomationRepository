3d23822f7ee6aeef48ce51b15c0cadc64ef13225
Dell <Dell@MyDELLPC>
2023-12-04 07:27:42 +0530

    First Commit

---
+++ TestingProject2/src/calculator/Calculator.java
@@ -0,0 +1,29 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        
+        System.out.println("Sum: " + (num1 + num2));
+        System.out.println("Difference: " + (num1 - num2));
+        System.out.println("Product: " + (num1 * num2));
+        
+        if (num2 != 0) {
+            System.out.println("Quotient: " + (num1 / num2));
+        } else {
+            System.out.println("Cannot divide by zero.");
+        }
+        
+        scanner.close();
+	}
+
+}
